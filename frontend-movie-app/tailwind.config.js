849ece0b9caa8f61e6ac5b9f19ec7387da1d526b
IbtissamB <ibtissam.bahaj@hotmail.com>
2025-09-18 15:00:00 +0300

    feat: initial setup of nextjs project

---
+++ frontend-movie-app/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        brand: {
+          bluegrey: "#2b3149",
+          accent: "#eb4e62",
+          lighttext: "#e9e9e9",
+          darktext: "#333333",
+          faint: "#f5f5f5",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
